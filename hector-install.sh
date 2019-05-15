b5261aec1128d2abf699c69c9916136bfabaa2a8
Valentin Hutter <valentinhutter1296@gmail.com>
2019-05-15 14:28:47 +0200

    - Forces the user to start the installation script as root - Configures the API key passed as a parameter in the .ini configuration file - Deletes the previous agent files (for reinstall)

--- hector-install.sh
+++ hector-install.sh
@@ -5,6 +5,8 @@
 # Date : 15.05.2019
 # Description : Diploma work - Hector agent monitoring - Installer
 # School : CFPT-I, Geneva, T.IS-E2 A
+# Help :
+#   - Using sed to replace text : https://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
 ########################################################################
 
 
@@ -15,11 +17,28 @@ COLOR_GREEN='\033[92m'
 COLOR_RED='\033[91m'
 COLOR_ORANGE='\033[93m'
 
+# Welcome text
+echo -e "${COLOR_BLUE}================================="
+echo -e "Hector agent installation script"
+echo -e "================================="
+echo -e "${COLOR_NC}"
+
+# Check that the installer is running as root
+if [ $(id -u) != "0" ]; then
+  echo -e "${COLOR_RED}Please run the install script as root. No worries, a new unprivileged user will be created just to run the agent itself.${COLOR_NC}"; 
+  exit 1;
+fi
+
 ### INSTALLER ###
 if [ "$1" != "" ]; then
+  echo -e "Downloading agent to /etc/hector-agent...";
+
   # Init agent folder
   if [ ! -d /etc/hector-agent ]; then
     mkdir -p /etc/hector-agent;
+  else
+    # New Install - Delete previous agent
+    rm -rf /etc/hector-agent/*
   fi
 
   # Retrieves the agent from the github repository and install it
@@ -41,6 +60,10 @@ if [ "$1" != "" ]; then
     echo -e "${COLOR_RED}An error occurred during the installation of the agent, please try again!${COLOR_NC}";
     exit 1
   fi
+
+  # Set API Token
+  echo -e "${COLOR_GREEN}Set API Token${COLOR_NC}";
+  sed -i'' -e "s/.*token.*/token = ${1}/g" /etc/hector-agent/hectoragent.ini
 
   # Sucessful installation
   echo -e "";
