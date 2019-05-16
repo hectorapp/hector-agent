ee3fd4a71fef928e996518dc233ef224867ecbae
Valentin Hutter <valentinhutter1296@gmail.com>
2019-05-16 13:33:02 +0200

    Creation of an unprivileged user dedicated to the agent (linux+darwin compatibility) Adding agent to our user's crontab

--- hector-install.sh
+++ hector-install.sh
@@ -6,7 +6,12 @@
 # Description : Diploma work - Hector agent monitoring - Installer
 # School : CFPT-I, Geneva, T.IS-E2 A
 # Help :
-#   - Using sed to replace text : https://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
+#   - Using sed to replace text: https://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
+#   - Create users on OSX:
+#       - https://apple.stackexchange.com/questions/274954/cannot-create-a-user-account-on-mac-using-command-line
+#       - https://pelaxa.com/blog/2015/03/17/creating-a-service-account-on-os-x-yosemite/
+#   - Explain chmod in detail: https://www.poftut.com/chmod-755-700/
+#   - Edit crontab from script: https://askubuntu.com/questions/880052/how-can-i-change-crontab-dynamically
 ########################################################################
 
 
@@ -31,7 +36,7 @@ fi
 
 ### INSTALLER ###
 if [ "$1" != "" ]; then
-  echo -e "Downloading agent to /etc/hector-agent...";
+  echo -e "${COLOR_ORANGE}Downloading agent to /etc/hector-agent...${COLOR_NC}";
 
   # Init agent folder
   if [ ! -d /etc/hector-agent ]; then
@@ -41,6 +46,8 @@ if [ "$1" != "" ]; then
     rm -rf /etc/hector-agent/*
   fi
 
+  echo -e "";
+  
   # Retrieves the agent from the github repository and install it
   cd /etc/hector-agent && wget --no-check-certificate --content-disposition https://github.com/valh1996/hector-agent/tarball/master -O hector-agent.tar.gz
   # Uncompress downloaded agent, and remove tar.gz
@@ -48,9 +55,12 @@ if [ "$1" != "" ]; then
   # Get the the just downloaded archive name (random)
   install_dirname=`find /etc/hector-agent -name "valh1996-hector-agent-*" -type d`
   # Copy the content of uncompressed archive into /etc/hector-agent and remove it
-  cp -a $install_dirname/. /etc/hector-agent/ && rm -rf $install_dirname
+  cp -a $install_dirname/. /etc/hector-agent && rm -rf $install_dirname
   # Remove hector-install.sh (useless, already installed)
   rm hector-install.sh
+  
+  echo -e "${COLOR_GREEN}Agent downloaded!${COLOR_NC}";
+  echo -e "";
 
   # Download agent's python dependencies
   if [ -e /etc/hector-agent/requirements.txt ]; then
@@ -61,9 +71,37 @@ if [ "$1" != "" ]; then
     exit 1
   fi
 
+  # Init logs directory
+  cd /etc/hector-agent && mkdir logs && > crontab.log
+
   # Set API Token
-  echo -e "${COLOR_GREEN}Set API Token${COLOR_NC}";
+  echo -e "${COLOR_GREEN}Set API Token...${COLOR_NC}";
   sed -i'' -e "s/.*token.*/token = ${1}/g" /etc/hector-agent/hectoragent.ini
+
+  # Create unpriviliged user
+  echo -e "${COLOR_GREEN}Create new user to run agent...${COLOR_NC}";
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    dscl . -create /Users/hectoragent
+    dscl . -create /Users/hectoragent uid 300
+    dscl . -create /Users/hectoragent gid 300
+    dscl . -create /Users/hectoragent NFSHomeDirectory /etc/hector-agent
+    dscl . -create /Users/hectoragent UserShell /usr/bin/false # Disable shell
+    dscl . -create /Users/hectoragent RealName "Hector Agent"
+    dscl . -create /Users/hectoragent passwd "*" # Special password linked to the group to prevent logins to the account
+  else
+    # New user named "hectoragent" with /etc/hector-agent home folder and shell login disabled
+    useradd hectoragent -r -d /etc/hector-agent -s /bin/false
+  fi
+
+  # Change user permissions
+  # User can read, write and execute, but group and others can't do anything
+  echo -e "${COLOR_GREEN}Set user permissions...${COLOR_NC}";
+  chown -R hectoragent: /etc/hector-agent && chmod -R 700 /etc/hector-agent
+  
+  # Register agent to crontab
+  cronlines="*/3 * * * * python3 bash /etc/hector-agent/hectoragent.py > /etc/hector-agent/logs/crontab.log 2>&1" # Redirect standard error (stderr) to crontab.log
+  echo "$cronlines" | crontab -u hectoragent - # Adding lines to crontab
 
   # Sucessful installation
   echo -e "";
