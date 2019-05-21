baa3bbd368c9d89cb36ede13515c4ef7b578f5d4
Valentin Hutter <valentinhutter1296@gmail.com>
2019-05-21 15:03:44 +0200

    The agent now verifies that it installed before collecting data

--- hectoragent.py
+++ hectoragent.py
@@ -50,45 +50,49 @@ class HectorAgent:
       config = configparser.ConfigParser()
       config.read('hectoragent.ini') # Loading configuration
       token = config['API']['token'].strip('"') # Get token from .ini configuration
+      is_agent_installed = config['GENERAL'].getboolean('installed')
       
-      # Collecting data
-      print('\nCollecting Data...')
-      mem = memory().collect()
-      swap_mem = swap().collect()
-      disk_usage_overall = disk().collect_overall_disk_usage()
-      disk_partitions = disk().collect_partitions()
-      disk_io = disk().collect_io()
-      net = network().collect_network_addrs()
-      server_loads = load().collect()
-      processes = process().collect()
-      extern_ping = ping().collect()
-      server_cpu = cpu().collect()
-      boot_time = float(psutil.boot_time())
+      if is_agent_installed:
+        # Collecting data
+        print('\nCollecting Data...')
+        mem = memory().collect()
+        swap_mem = swap().collect()
+        disk_usage_overall = disk().collect_overall_disk_usage()
+        disk_partitions = disk().collect_partitions()
+        disk_io = disk().collect_io()
+        net = network().collect_network_addrs()
+        server_loads = load().collect()
+        processes = process().collect()
+        extern_ping = ping().collect()
+        server_cpu = cpu().collect()
+        boot_time = float(psutil.boot_time())
 
-      # Sending data to the API
-      try:
-        print('Sending data to API...')
-        res = requests.post(API_ENDPOINT + "/servers?server_token=" + token, data={
-          'os_fullname': self._server_platform(),
-          'boot_time': boot_time,
-          'memory': helpers.dict_to_base64(mem),
-          'swap': helpers.dict_to_base64(swap_mem),
-          'disk_usage_overall': helpers.dict_to_base64(disk_usage_overall),
-          'disk_partitions': helpers.dict_to_base64(disk_partitions),
-          'disk_io': helpers.dict_to_base64(disk_io),
-          'network': helpers.dict_to_base64(net),
-          'loads': helpers.dict_to_base64(server_loads),
-          'processes': helpers.dict_to_base64(processes),
-          'extern_ping': helpers.dict_to_base64(extern_ping),
-          'cpu': helpers.dict_to_base64(server_cpu),
-          'request_send_at': datetime.datetime.now(),
-        })
-        
-        if res.status_code == 200 and json.loads(res.text)['success']:
-          print(colors.GREEN + '\nThe data has been correctly sent to the API!' + colors.NORMAL)
+        # Sending data to the API
+        try:
+          print('Sending data to API...')
+          res = requests.post(API_ENDPOINT + "/servers?server_token=" + token, data={
+            'os_fullname': self._server_platform(),
+            'boot_time': boot_time,
+            'memory': helpers.dict_to_base64(mem),
+            'swap': helpers.dict_to_base64(swap_mem),
+            'disk_usage_overall': helpers.dict_to_base64(disk_usage_overall),
+            'disk_partitions': helpers.dict_to_base64(disk_partitions),
+            'disk_io': helpers.dict_to_base64(disk_io),
+            'network': helpers.dict_to_base64(net),
+            'loads': helpers.dict_to_base64(server_loads),
+            'processes': helpers.dict_to_base64(processes),
+            'extern_ping': helpers.dict_to_base64(extern_ping),
+            'cpu': helpers.dict_to_base64(server_cpu),
+            'request_send_at': datetime.datetime.now(),
+          })
+          
+          if res.status_code == 200 and json.loads(res.text)['success']:
+            print(colors.GREEN + '\nThe data has been correctly sent to the API!' + colors.NORMAL)
 
-      except requests.exceptions.RequestException:
-        sys.exit(1)
+        except requests.exceptions.RequestException:
+          sys.exit(1)
+      else:
+        print(colors.RED + "The agent does not seem to be correctly installed, please restart the installation script!" + colors.NORMAL)
     
   # Start the agent
   def start(self):
