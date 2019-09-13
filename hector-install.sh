509af20de354f83706dc1441d011d50127e34a67
root <root@virt2.teedesignshop.com>
2019-09-14 01:08:44 +0200

    Local changes for testing

--- hector-install.sh
+++ hector-install.sh
@@ -191,11 +191,11 @@ if [ "$1" != "" ]; then
   fi
 
   # Retrieves the agent from the github repository and install it
-  cd $INSTALLATION_PATH && wget --no-check-certificate --content-disposition https://github.com/valh1996/hector-agent/tarball/master -O hector-agent.tar.gz
+  cd $INSTALLATION_PATH && wget --no-check-certificate --content-disposition https://github.com/SibirskiyVolk/hector-agent/tarball/master -O hector-agent.tar.gz
   # Uncompress downloaded agent, and remove tar.gz
   tar -zxvf hector-agent.tar.gz && rm $INSTALLATION_PATH/hector-agent.tar.gz
   # Get the the just downloaded archive name (random)
-  install_dirname=`find $INSTALLATION_PATH -name "valh1996-hector-agent-*" -type d`
+  install_dirname=`find $INSTALLATION_PATH -name "SibirskiyVolk-hector-agent-*" -type d`
   # Copy the content of uncompressed archive into /opt/hector-agent and remove it
   cp -a $install_dirname/. $INSTALLATION_PATH && rm -rf $install_dirname
 
