b15370dbd3cb65ea525fe4b6d4938655f8155df3
Valentin Hutter <valentinhutter1296@gmail.com>
2020-04-14 16:32:00 +0200

    Add geoip to IP module

--- modules/ip.py
+++ modules/ip.py
@@ -5,53 +5,66 @@
  * Author : Hutter Valentin
  * Date : 22.05.2019
  * Description : Hector agent monitoring
- * Help :
-    - Get public ip with DNS server : 
-      - https://www.cyberciti.biz/faq/how-to-find-my-public-ip-address-from-command-line-on-a-linux/
-      - https://unix.stackexchange.com/questions/477705/resolve-my-ip-with-dig-returns-empty-string
-    - Linux IP : https://stackoverflow.com/questions/21336126/linux-bash-script-to-extract-ip-address
 '''
 
 import sys
 import psutil
+import requests
 from subprocess import Popen, PIPE
 sys.path.insert(0, '..') # to import helpers from parent folder
 import helpers
 
 class ip:
 
-  def _exec_command_ip(self, command, private):
-    data = {}
+  def _get_ip_data(self, private = True):
+    data = {
+      'ip': None,
+      'is_private': private,
+      'country_code': None,
+      'country_name': None,
+      'latitude': None,
+      'longitude': None,
+      'region_code': None,
+      'region_name': None
+    }
 
     try:
-      private_ip = Popen(command, shell=True, stdout=PIPE)
-      output = private_ip.communicate()[0]
+      if private:
+        command = ''
 
-      if output:
-        data = {
-          'ip': output.decode("utf-8").strip().strip('"'),
-          'is_private': private,
-        }
+        # Generates command depending on OS
+        if psutil.MACOS:
+          command = 'ipconfig getifaddr en0'
+        else:
+          command = "hostname -I | awk '{ print $1 }'"
 
+        private_ip = Popen(command, shell=True, stdout=PIPE)
+        output = private_ip.communicate()[0]
+
+        if output:
+          data['ip'] = output.decode("utf-8").strip().strip('"')
+      else:
+        res = requests.get(url='https://freegeoip.app/json')
+        json = res.json()
+
+        if json:
+          data['ip'] = json['ip']
+          data['country_code'] = json['country_code']
+          data['country_name'] = json['country_name']
+          data['latitude'] = json['latitude']
+          data['longitude'] = json['longitude']
+          data['region_code'] = json['region_code']
+          data['region_name'] = json['region_name']
     except:
       pass
 
     return data
 
   def public_ip(self):
-    # Using a DNS server to retrieve the public IP
-    return self._exec_command_ip('dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com', False)
+    return self._get_ip_data(False)
 
   def private_ip(self):
-    command = ''
-    
-    # Generates command depending on OS
-    if psutil.MACOS:
-      command = 'ipconfig getifaddr en0'
-    else:
-      command = "hostname -I | awk '{ print $1 }'"
-
-    return self._exec_command_ip(command, True)
+    return self._get_ip_data(True)
 
   def collect_ips(self):
     # Collect public ip 
